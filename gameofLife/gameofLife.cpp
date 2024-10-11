2da22585bf81b66b08a05b62fdc06907724fff11
Cat <c.k.appleby1@newcastle.ac.uk>
2024-10-11 14:13:28 +0100

    Creates inital method for game setup - not yet completed

--- gameofLife/gameofLife.cpp
+++ gameofLife/gameofLife.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int GRID_SIZE;
+    int STARTING_CELL_AMOUNT;
+    int GAME_TIME;
+
+    cout << "Enter grid size: " << "\n";
+    cin >> GRID_SIZE;
+
+    cout << "Enter amount of starting cells: " << "\n";
+    cin >> STARTING_CELL_AMOUNT;
+
+    cout << "Enter amount of turns: ";
+    cin >> GAME_TIME;
+
+}
+
+void makeGrid(int GRID_SIZE, int STARTING_CELL_AMOUNT)
+{
+
 }
