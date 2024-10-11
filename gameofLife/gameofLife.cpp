7b17523cbf503f6902443988b39c83d93137e6f7
Cat <c.k.appleby1@newcastle.ac.uk>
2024-10-11 14:43:22 +0100

    Correctly formatted grid is now created.

--- gameofLife/gameofLife.cpp
+++ gameofLife/gameofLife.cpp
@@ -2,11 +2,33 @@
 
 using namespace std;
 
+void makeGrid(int GRID_SIZE, int STARTING_CELL_AMOUNT)
+{
+    // Create necessary memory
+    char** array = new char* [GRID_SIZE]; 
+    for (int i = 0; i < GRID_SIZE; ++i) {
+        array[i] = new char[GRID_SIZE]; 
+    }
+    
+    // Create the actual grid
+    for (int i = 0; i < GRID_SIZE; ++i) {
+        for (int j = 0; j < GRID_SIZE; ++j) {
+            array[i][j] = '.'; 
+        }
+    }
+    
+    // Print the grid
+    for (int i = 0; i < GRID_SIZE; ++i) {
+        for (int j = 0; j < GRID_SIZE; ++j) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl; 
+    }
+}
+
 int main()
 {
-    int GRID_SIZE;
-    int STARTING_CELL_AMOUNT;
-    int GAME_TIME;
+    int GRID_SIZE, STARTING_CELL_AMOUNT, GAME_TIME;
 
     cout << "Enter grid size: " << "\n";
     cin >> GRID_SIZE;
@@ -17,9 +39,8 @@ int main()
     cout << "Enter amount of turns: ";
     cin >> GAME_TIME;
 
-}
-
-void makeGrid(int GRID_SIZE, int STARTING_CELL_AMOUNT)
-{
+    makeGrid(GRID_SIZE, STARTING_CELL_AMOUNT);
 
 }
+
+
