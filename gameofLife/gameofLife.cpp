777590b759c5a498494c3a4b012400063a5022c7
c1009401 <c.k.appleby1@newcastle.ac.uk>
2024-10-14 14:32:58 +0100

    Redone

--- gameofLife/gameofLife.cpp
+++ gameofLife/gameofLife.cpp
@@ -1,50 +1,76 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-void makeGrid(int GRID_SIZE, int STARTING_CELL_AMOUNT)
-{
-    int rows = GRID_SIZE;
-    int columns = GRID_SIZE;
+int GRID_SIZE, STARTING_CELL_AMOUNT, GAME_TIME, ROWS, COLUMNS;
 
-    // Create necessary memory
-    char** array = new char* [rows]; 
-    for (int i = 0; i < rows; ++i) {
-        array[i] = new char[columns]; 
+class Cell
+{
+    bool isAlive;
+
+public:
+    Cell()
+    {
+        isAlive = false;
     }
-    
-    // Create the actual grid
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
-            array[i][j] = ' '; 
+};
+
+class Grid
+{
+    Cell** board;
+    int rows;
+    int columns;
+
+public:
+    Grid(int ROWS, int COLUMNS)
+    {
+        rows = ROWS;
+        columns = COLUMNS;
+
+        board = new Cell* [rows];
+        for (int i = 0; i < rows; i++)
+        {
+            board = new Cell* [columns];
         }
     }
-    
-    // Print the grid
-    for (int i = 0; i < rows; ++i) {
-        cout << ".";
-        for (int j = 0; j < columns; ++j) {
-            cout << array[i][j] << ".";
+};
+
+
+void saveGame()
+{
+    cout << "Saving game";
+    ofstream my_file("save_data.csv");
+
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLUMNS; ++j) {
+            cin >> CURRENT_GRID[i][j];
+            my_file << "." << CURRENT_GRID[i][j] << ",";
         }
-        cout << endl; 
     }
+    my_file.close();
+    cout << "File saved";
 }
+
 
 int main()
 {
-    int GRID_SIZE, STARTING_CELL_AMOUNT, GAME_TIME;
-
-    cout << "Enter grid size: " << "\n";
-    cin >> GRID_SIZE;
+    cout << "Enter amount of rows: " << "\n";
+    cin >> ROWS;
+    
+    cout << "Enter amount of height: " << "\n";
+    cin >> COLUMNS;
 
     cout << "Enter amount of starting cells: " << "\n";
     cin >> STARTING_CELL_AMOUNT;
 
-    cout << "Enter amount of turns: ";
+    cout << "Enter amount of turns: " << "\n";
     cin >> GAME_TIME;
 
-    makeGrid(GRID_SIZE, STARTING_CELL_AMOUNT);
+    Grid game_board(ROWS, COLUMNS);
+    
 
+    return 0;
 }
 
 
