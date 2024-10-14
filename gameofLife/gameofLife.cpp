fa41d1f5d8259a410ef5e4ad3030d3b4a99e9964
Cat <c.k.appleby1@newcastle.ac.uk>
2024-10-14 17:47:35 +0100

    Updated comments

--- gameofLife/gameofLife.cpp
+++ gameofLife/gameofLife.cpp
@@ -119,6 +119,8 @@ public:
 private:
     int countAliveNeighbors(int x, int y) {
         int count = 0;
+
+        // Checks all surrounding cells for alive neighbours
         for (int i = -1; i <= 1; ++i) {
             for (int j = -1; j <= 1; ++j) {
                 if (i == 0 && j == 0) continue; // Skip the cell itself
