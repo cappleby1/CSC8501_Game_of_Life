69b39e74e212f58408c977c60f1ac00fcca76351
c1009401 <c.k.appleby1@newcastle.ac.uk>
2024-10-16 13:15:19 +0100

    Added block & behive working

--- gameofLife/gameofLife.cpp
+++ gameofLife/gameofLife.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int STARTING_CELL_AMOUNT, GAME_TIME, ROWS, COLUMNS;
+int STARTING_CELL_AMOUNT, GAME_TIME, ROWS, COLUMNS, CURRENT_TURN;
 
 class Cell {
     bool is_alive = false;
@@ -150,21 +150,69 @@ public:
     bool checkForBlock()
     {
         // Check for each possible 2x2 grid
-        for (int i = 0; i < rows -1; ++i) {
-            for (int j = 0; j < columns -1; ++j) {
+        for (int i = 0; i < rows -1; ++i) 
+        {
+            for (int j = 0; j < columns -1; ++j) 
+            {
                 bool cell1 = board[i][j].isAliveState();
                 bool cell2 = board[i][j + 1].isAliveState();
                 bool cell3 = board[i + 1][j].isAliveState();
                 bool cell4 = board[i + 1][j + 1].isAliveState();
 
+                bool top_dead = (i == 0) || !board[i - 1][j].isAliveState() && !board[i - 1][j + 1].isAliveState();
+                bool left_dead = (j == 0) || !board[i][j - 1].isAliveState();
+                bool right_dead = (j + 1 == columns - 1) || !board[i][j + 2].isAliveState();
+                bool bottom_dead = (i + 1 == rows - 1) || !board[i + 2][j].isAliveState() && !board[i + 2][j + 1].isAliveState();
+
+
                 // Check the 2x2 grid
-                if (cell1 == true && cell1 == cell2 && cell3 == cell2 && cell3 == cell4)
+                if (cell1 && cell2 && cell3 && cell4 && top_dead && left_dead && right_dead && bottom_dead)
                 {
-                    return true; // Found a matching 2x2 grid
+                    // Found a matching 2x2 grid
+                    return true;
                 }
             }
         }
         return false;
+    }
+
+    bool checkForBeehive()
+    {
+        for (int i = 0; i < rows - 3; ++i)
+        {
+            for (int j = 0; j < columns - 2; ++j)
+            {
+                bool cell1 = board[i][j].isAliveState();
+                bool cell2 = board[i + 1][j].isAliveState();
+                bool cell3 = board[i + 2][j].isAliveState();
+                bool cell4 = board[i + 3][j + 1].isAliveState();
+                bool cell5 = board[i + 1][j + 2].isAliveState();
+                bool cell6 = board[i + 2][j + 2].isAliveState();
+                bool cell7 = board[i][j + 1].isAliveState();
+
+
+                if (!cell1 && cell2 && cell3 && cell4 && cell5 && cell6 && cell7)
+                {
+                    // Found a beehive
+                    return true; 
+                }
+            }
+        }
+        return false;
+
+    }
+
+    bool areAllCellsDead()
+    {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                if (board[i][j].isAliveState()) { 
+                    // Found a live cell
+                    return false; 
+                }
+            }
+        }
+        return true; 
     }
 
 private:
@@ -226,14 +274,33 @@ void createNewGame()
 
     // Question 2
     case 2:
-        for (int i = 0; i < GAME_TIME; ++i)
+        bool found = false;
+        CURRENT_TURN = 0;
+        while(!found)
         {
-            cout << "\n Turn " << (i + 1) << ":\n";
+
+            if (game_board.areAllCellsDead())
+            {
+                cout << "\n" << "All cells dead";
+                break;
+            }
+
+            CURRENT_TURN += 1;
+            cout << "\n Turn " << CURRENT_TURN << ":\n";
             game_board.updateGrid();
 
             if (game_board.checkForBlock())
             {
+                cout << "\n Turn " << CURRENT_TURN << ":\n";
                 cout << "\n" << "2x2 found" << ":\n";
+                game_board.printGrid();
+                break;
+            }
+            
+            if (game_board.checkForBeehive())
+            {
+                cout << "\n Turn " << CURRENT_TURN << ":\n";
+                cout << "\n" << "beehive found" << ":\n";
                 game_board.printGrid();
                 break;
             }
